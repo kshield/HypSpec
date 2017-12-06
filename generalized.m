fce2c482a323c0e65966649e8a025c9985bc923e
kshield <kshield@berkeley.edu>
2017-12-05 16:28:12 -0800

    'attempt the first at plotting; sorta works for one pH, fails for multiple'

--- generalized.m
+++ generalized.m
@@ -1,14 +1,9 @@
 function Y=generalized(filename, concentrations)
-
-%filename in ' .txt' form
+%filename in 'C:/Users/kathe/Desktop/Berkeley/AbergelGroup/Data/20160922CHHC2.txt' form
 %concentrations in [ ] form; space delimited
 %% Importing
-% Import the data somehow, magically.
 % Save as "XXXX" (where X = C (CAM) or H (HOPO)
-
-filename = 'C:/Users/kathe/Desktop/Berkeley/AbergelGroup/Data/20160922CHHC2.txt';
 delimiter = '\t';
-%concentrations = [3,4,16]
 
 A = importdata(filename,delimiter);
 
@@ -20,77 +15,44 @@ fulldata = cat(1, titles, data);
 
 %% corrections
 
-%volume - adjusting for incorrect initial volume of 0.02 instead of 0.00
+%VOLUME - 
+% adjusting for incorrect initial volume of 0.02 instead of 0.00
 fulldata(2:end,2) = fulldata(2:end,2)-0.02;
 
-%pH - correcting the fact that the first data point's pH is a holdover from
-%the last time the software was run. adjusting to have same difference as
-%the next pH change.
+%pH - 
+% correcting the fact that the first data point's pH is a holdover from
+% the last time the software was run. adjusting to have same difference as
+% the next pH change.
 fulldata(2,3) = fulldata(3,3)-(fulldata(4,3)-fulldata(3,3));
 
 %% id
+id = fulldata(2:end,1)';
 % Isolate the points (column one) as a matrix called "id" [0]
 % Transpose
 % Doesn't keep the header
-id = fulldata(2:end,1)';
-% % str2num(id{1})
-% XXXX(2:end,1)';
-
-
-
 %% pH
+pH=fulldata(2:end,3)';
 % Isolate the pH (column three); call it "pH" 
 % Transpose
 % Doesn't keep the header
-pH=fulldata(2:end,3)';
-
-
 %% Create a path row "path" [1] 
+path = ones(1,size(fulldata,1)-1); 
 % = 1cm unless otherwise noted
-path=ones(1,size(fulldata,1)-1); 
-
-
+% IF NOT 1CM:
+% path = zeros(1,size(fulldata,1)-1) + PATHLENGTH IN CM;
 %% Create the Concentration matrix [2]
-% N = nargin;
-% %c = length(varargin);
-% 
-% if N = 1
-%          conc = zeros(1,size(fulldata,1)) + varargin{1};
-% elseif N = 3
-%         conc = [zeros(1,size(fulldata,1)) + varargin{1}; zeros(1,size(fulldata,1)) + varargin{2}];
-% elseif N = 4 
-%         conc = [zeros(1,size(fulldata,1)) + varargin{1}; zeros(1,size(fulldata,1)) + varargin{2}; zeros(1,size(fulldata,1)) + varargin{3}];
-% end
-
 conc1 = zeros(length(concentrations),size(fulldata,1));
-% 
-% conc1 = conc(1,:);
-% %varargin{k}
-% 
-% for k = 1:c
-%     conc(k,:) = varargin{k};
-%     
-% end 
-
+% matrix of size: 
+% # of rows = # of entries in the concentrations vector
+% # of columns = width of fulldata matrix; 
 for i = 1:length(concentrations)
     conc1(i,:) = concentrations(i);
 end
-
-%conc = zeros(length(varargin),size(fulldata,1))
-
-%for varargin{i}
- %   conc(i,:) = varargin(i)
-%end
-
 %% Create the wavelength matrix
 wavelength=fulldata(:,4:end)';
-
-
+% vast majority of the data; all the wavelengths & associated data points
 %% Combine into one Matrix
 dataexport=[0 id;1 path;conc1;3 pH; wavelength];
-%and additional concentrations as appropriate%
-
-
 %% Get rid of the bottom crap
 for i = 0:length(dataexport)
     j = length(dataexport);
@@ -106,19 +68,10 @@ for i = 0:length(dataexport)
     end
     
 end
-
-
-%%conc = zeros(length(concentrations),size(fulldata,1));
-
-% 
-% minimum = dataexport(5:end,2:end);
-% minimum = min(minimum,[],1);
-% 
-
 %% Adjust values from minimum
-size(conc1,1)+4
 minmat = dataexport(size(conc1,1)+4:end,2:end); 
-%has to be dataexport matrix b/c lost the random stuff at the bottom
+% has to be dataexport matrix b/c lost the random stuff at the bottom
+% minimums matrix of the size of the cleaned up 
 mins = min(minmat,[],1);
 finaldata = zeros(size(dataexport,1), size(dataexport,2));
 
@@ -129,20 +82,37 @@ end
 
 for k = 1:size(conc1,1)+3
     finaldata(k,:) = dataexport(k,:);    
-end
-
-    
+end    
 finaldata(:,1)=dataexport(:,1);
-
-
 
 %% Saving the file
 Y=finaldata;
 saveas = sprintf('%s_out.txt', filename);
 dlmwrite(saveas, Y,' ');
-% %Problem ^ this overwrites the original file%
-% 
-% 
+
+%% plots
+%plot(pH,mins)
+%for i = 7:size(dataexport,1)
+ %  for j = 2:size(dataexport,2)
+  %    plot(dataexport(i,1),dataexport(i,j))
+   %end
+%end
+
+
+%% plot take two : 
+for i = length(concentrations)+4:size(dataexport,1)
+   x = dataexport(i,1);
+  % y = dataexport(i,2);
+  % scatter(x,y)
+  % hold on
+   for j = 2:size(dataexport,2)
+      y = dataexport(i,j);
+      scatter (x,y)
+      hold on
+   end
+end
+
+hold off
 
 end
 
